5d7ff18d61069f45475c1e0d5bb91219652e8649
Triginarsa <mankadi123@gmail.com>
2021-05-16 04:36:46 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.svelte
@@ -0,0 +1,61 @@
+<script>
+	import { fade,fly } from 'svelte/transition'
+	import EmojiDisplay from './EmojiDisplay.svelte';
+	import EmojiDescription from './EmojiDescription.svelte';
+	import Button from './Button.svelte'
+
+	let isLoaded = false;
+	let currentEmoji = '😃';
+	const emojis = ['🤣', '🐵', '🖕', '🚀'];
+	let m = { x:0, y:0};
+
+	function randomizeEmoji() {
+		return emojis[Math.floor(Math.random() * emojis.length)];
+	}
+
+	function handleRandomButton() {
+		currentEmoji = randomizeEmoji()
+	}
+
+	setTimeout(function() {
+		isLoaded = true;
+	}, 2500);
+
+	function handleMousemove(event) {
+		m.x = event.clientX;
+		m.y = event.clientY;
+	}
+</script>
+
+<style>
+	div {
+		margin: 2em;
+	}
+</style>
+
+<svelte:head>
+	<link href="/terminal.min.css" rel="stylesheet" />
+</svelte:head>
+
+<div class="container" on:mousemove={handleMousemove}>
+<p>
+	The mouse position: {m.x} x {m.y}
+</p>
+	<h1>Randomize Emoji</h1>
+	<ul>
+		{#each emojis as emoji}
+		<li>{emoji}</li>
+		{/each}
+	</ul>
+	{#if isLoaded === true}
+	<div in:fly={{y:200, duration:2000}} out:fade>
+	<EmojiDisplay {currentEmoji} />
+	<EmojiDescription />
+	<Button on:click={handleRandomButton} title={'🔁 Randomize'} />
+	</div>
+	{:else}
+	<h2>Loading...</h2>
+	{/if}
+
+	<Button title={'Toggle'} on:click={() => isLoaded = !isLoaded}></Button>
+</div>
---
+++ src/EmojiDescription.svelte
@@ -0,0 +1,49 @@
+<script>
+    import {onMount, onDestroy, beforeUpdate, afterUpdate} from 'svelte';
+
+    let description = '';
+    $: characters = description.length;
+    $: words = description.split(' ').length;
+
+    $: if (description.length > 10){
+        console.log(`---------`)
+        console.log(`Description: ${description}`)
+        }
+
+    function handleSubmit(event){
+        console.log('form submitted. Thanks!')
+        description = '';
+    }
+
+    onMount(function (){
+        console.log('onMount');
+    });
+    onDestroy(function(){
+        console.log('onDestroy');
+    });
+    beforeUpdate(function(){
+        console.log('beforeUpdate');
+    });
+    afterUpdate(function(){
+        console.log('afterUpdate');
+    });
+
+</script>
+
+<style>
+    div {
+        margin-top: 1em;
+        margin-bottom: 1em;
+    }
+</style>
+
+<div class="form-group">
+    <p>{description}</p>
+    <form on:submit|preventDefault={handleSubmit}>
+        <textarea bind:value={description} rows="7" />
+        <button type="submit">Save</button>
+    </form>
+    
+    <p>Character count: {characters}</p>
+    <p>Word count: {words}</p>
+</div>
